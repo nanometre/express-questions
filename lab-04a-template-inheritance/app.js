a87fce2909dfd6b401031d94245c02f961a527fc
Benedict Wong <benedictwcy@hotmail.com>
2022-02-18 16:34:45 +0800

    Completed lab-04a

---
+++ lab-04a-template-inheritance/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const wax = require('wax-on');
+const hbs = require('hbs');
+var helpers = require('handlebars-helpers')({
+  handlebars: hbs.handlebars
+});
+const path = require('path');
+let app = express();
+
+app.set('view engine', hbs);
+app.set('views', path.join(__dirname, '/views'));
+
+wax.on(hbs.handlebars);
+wax.setLayoutPath(path.join(__dirname, '/views/layouts'))
+
+// YOUR CODE HERE
+app.get('/', function(req,res){
+  res.render('index.hbs')
+})
+
+module.exports = app;
---
+++ lab-05a-form-processing/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const wax = require('wax-on');
+const hbs = require('hbs');
+var helpers = require('handlebars-helpers')({
+  handlebars: hbs.handlebars
+});
+const path = require('path');
+let app = express();
+
+app.set('view engine', hbs);
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// YOUR CODE HERE
+app.get('/form1', function(req,res){
+  res.render('form1')
+})
+
+module.exports = app;
